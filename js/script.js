54bca285eb07e7beb9a1d26bdfeec8f525f070ec
DESKTOP-NS8HAV3 <may_aj@outlook.com>
2023-10-25 11:42:36 +0200

    finished task

--- js/script.js
+++ js/script.js
@@ -4,39 +4,31 @@ const cardDrawn = document.querySelector('#cardDrawn');
 
 const url = 'https://deckofcardsapi.com/api/deck/new/draw/?count=1';
 
-const getCardOnClick = () => {
-    cardImg.innerHTML = '';
-    
-    fetch(url, {
+const getCardOnClick = async () => {
+    const res = await fetch(url, {
         method: 'GET',
         headers: {
             'Accept': 'application/json'
         }
-    })
-        .then(res => { if(res.ok) return res.json()})
-        .then(data => {
-            console.log(data)
-            
-            const cardImgSrc = data.cards[0].image;
-            const suit = data.cards[0].suit;
-            const value = data.cards[0].value;
-            let img = document.createElement('img');
-            img.setAttribute('src', cardImgSrc);
-            cardImg.appendChild(img);
-            //showNewPic(img);
+    });
 
-            cardDrawn.innerHTML = `You got ${value} of ${suit}`
-        });
+    const data = await res.json();
+
+    const cardImgSrc = data.cards[0].image;
+    const suit = data.cards[0].suit;
+    const value = data.cards[0].value;
+
+    let img = document.createElement('img');
+    img.src = cardImgSrc;
+
+    changeImg(img);
+    cardDrawn.innerHTML = `You got ${value} of ${suit}!`
 }
 
-async function removePic() {
-    // if(cardImg.childElementCount > 0) {
-    //     cardImg.removeChild(cardImg.firstChild);
-        
-    // }
-    //cardImg.removeChild(cardImg.firstChild);
-
-    
+function changeImg(img) {
+    if(cardImg.hasChildNodes()) {
+        cardImg.removeChild(cardImg.firstChild);
+    }
+    cardImg.appendChild(img);
 }
-
 button.addEventListener('click', getCardOnClick);
